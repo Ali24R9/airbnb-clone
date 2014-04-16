16f1cee0069cabf0f024a5eb039acbeb4829d21f
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2014-04-16 10:03:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airbnb::Application.config.secret_key_base = 'd2ede3bab5217a1ed23fb5c578f3ab2d57aa7f9d57d482f6fd1b423e1f8afa117a984046135c10cf4d0bafed3fc58107cc7af8c1b833ff0f239ae2778d5c2cb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Airbnb::Application.config.session_store :cookie_store, key: '_airbnb_session'
